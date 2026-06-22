c934256cb4d76b2d0d3d9ba65a51977ccd3230e3
Marco Antonio Bonifacio Ruiz <60863027@continental.edu.pe>
2026-06-22 16:11:11 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -13,9 +13,15 @@ export default defineConfig(() => {
         '@': path.resolve(__dirname, '.'),
       },
     },
+    // 🛠️ AGREGAMOS ESTE BLOQUE AQUÍ: Evita que el empaquetador del navegador intente leer el backend
+    build: {
+      rollupOptions: {
+        external: ['server.ts', 'express', 'dotenv', '@google/genai'],
+      },
+    },
     server: {
       // Si DISABLE_HMR es true, pasamos false para apagar el WebSocket por completo
-      hmr: isHmrDisabled ? false : { protocol: 'ws', host: 'localhost' },
+      hmer: isHmrDisabled ? false : { protocol: 'ws', host: 'localhost' },
       // Desactiva el guardado/observador de archivos si HMR está deshabilitado
       watch: isHmrDisabled ? null : {},
     },
